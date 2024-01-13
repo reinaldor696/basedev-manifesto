fd3747f910f788dce214ce2a0171bd219e044acf
Reinaldo Romero <reinaldoromero696@gmail.com>
2024-01-13 19:50:13 -0400

    commit fourteenth

--- js/scrollTrigger.js
+++ js/scrollTrigger.js
@@ -3,13 +3,32 @@ Init ScrollTrigger // Inicio de ScrollTrigger
 ------------------------------*/
 const tl = gsap.timeline({
 scrollTrigger: {
-    trigger: ".trigger",
-    start: "660 top",
-    end: "+=1500",
+    trigger: ".img-container",
+    start: "200 top",
+    end: "bottom",
     scrub: 1,
     pin: true,
     markers: true
 }
 });
-tl.to("#img", {width: '100%', duration: 1})
-tl.to("#img", {opacity: '0.2', duration: 3})
+tl.to("#img", {   
+    position: 'center',
+    width: '100%', 
+    duration: 1,
+})
+
+tl.to(".img-container-par", {
+    y: '-600px', 
+    duration: 1
+}, '-=0.9')
+
+gsap.to(".img-container-par p", {
+    backgroundPositionX: "0%",
+    stagger: 1,
+    scrollTrigger: {
+        trigger: ".img-container-par p",
+        start: "top center",
+        end: "bottom top",
+        scrub: 1
+    }
+})
